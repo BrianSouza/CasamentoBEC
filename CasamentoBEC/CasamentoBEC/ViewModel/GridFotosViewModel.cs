50263e4b2ebd77bb7f96804a4aa3d1b76dd1285c
Brian Diego de Souza <Brian-desouza@hotmail.com>
2019-01-28 21:35:29 -0200

    tentando arrumar o grid de imagens

--- CasamentoBEC/CasamentoBEC/ViewModel/GridFotosViewModel.cs
+++ CasamentoBEC/CasamentoBEC/ViewModel/GridFotosViewModel.cs
@@ -36,7 +36,12 @@ namespace CasamentoBEC.ViewModel
         {
             ImagesSources = new List<Fotos>()
             {
-               new Fotos(){ ImgSource = GetImageSource("https://abrilveja.files.wordpress.com/2016/09/istock_82086545_large.jpg")},
+                new Fotos(){ ImgSource = GetImageSource("https://abrilveja.files.wordpress.com/2016/09/istock_82086545_large.jpg")},
+                new Fotos(){ ImgSource =GetImageSource("https://www.papeleestilo.com.br/wp-content/uploads/2018/09/fotos-de-casamento-01-e1536947656634.jpg") },
+                new Fotos(){ ImgSource =GetImageSource("https://cdn0.casamentos.com.br/img_e_163086/3/0/8/6/t30_caroline-roger-mini-wedding-casamento-intimista-rio-negrinho-fotografo-sc-boho-ganske-fotos-e-historias-41_13_163086_v1.jpg") },
+                new Fotos(){ ImgSource =GetImageSource("https://inesquecivelcasamento.s3.amazonaws.com/wp-content/uploads/2018/05/casamento_niina_gui-187-IMG_1345-660x400.jpg") },
+                new Fotos(){ ImgSource =GetImageSource("http://s2.glbimg.com/RhRreT6XqgjneN8bmSTN4DeMdCc=/smart/e.glbimg.com/og/ed/f/original/2015/05/21/casamento-abre.jpg") },
+                new Fotos(){ ImgSource = GetImageSource("https://abrilveja.files.wordpress.com/2016/09/istock_82086545_large.jpg")},
                 new Fotos(){ ImgSource =GetImageSource("https://www.papeleestilo.com.br/wp-content/uploads/2018/09/fotos-de-casamento-01-e1536947656634.jpg") },
                 new Fotos(){ ImgSource =GetImageSource("https://cdn0.casamentos.com.br/img_e_163086/3/0/8/6/t30_caroline-roger-mini-wedding-casamento-intimista-rio-negrinho-fotografo-sc-boho-ganske-fotos-e-historias-41_13_163086_v1.jpg") },
                 new Fotos(){ ImgSource =GetImageSource("https://inesquecivelcasamento.s3.amazonaws.com/wp-content/uploads/2018/05/casamento_niina_gui-187-IMG_1345-660x400.jpg") },
