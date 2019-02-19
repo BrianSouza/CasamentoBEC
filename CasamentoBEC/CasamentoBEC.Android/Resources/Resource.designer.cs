51effb14314b3ca419027303b927b46fbb4a3636
Brian Diego de Souza <Brian-desouza@hotmail.com>
2019-02-18 21:46:17 -0300

    desenvolvendo layout rsvp

--- CasamentoBEC/CasamentoBEC.Android/Resources/Resource.designer.cs
+++ CasamentoBEC/CasamentoBEC.Android/Resources/Resource.designer.cs
@@ -2204,26 +2204,26 @@ namespace CasamentoBEC.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020129
-			public const int avd_hide_password_1 = 2130837801;
-			
 			// aapt resource value: 0x7f02012a
-			public const int avd_hide_password_2 = 2130837802;
+			public const int avd_hide_password_1 = 2130837802;
 			
 			// aapt resource value: 0x7f02012b
-			public const int avd_hide_password_3 = 2130837803;
+			public const int avd_hide_password_2 = 2130837803;
+			
+			// aapt resource value: 0x7f02012c
+			public const int avd_hide_password_3 = 2130837804;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f02012c
-			public const int avd_show_password_1 = 2130837804;
-			
 			// aapt resource value: 0x7f02012d
-			public const int avd_show_password_2 = 2130837805;
+			public const int avd_show_password_1 = 2130837805;
 			
 			// aapt resource value: 0x7f02012e
-			public const int avd_show_password_3 = 2130837806;
+			public const int avd_show_password_2 = 2130837806;
+			
+			// aapt resource value: 0x7f02012f
+			public const int avd_show_password_3 = 2130837807;
 			
 			// aapt resource value: 0x7f020055
 			public const int backgrounddetail = 2130837589;
@@ -2843,11 +2843,11 @@ namespace CasamentoBEC.Droid
 			// aapt resource value: 0x7f020122
 			public const int notification_icon_background = 2130837794;
 			
-			// aapt resource value: 0x7f020127
-			public const int notification_template_icon_bg = 2130837799;
-			
 			// aapt resource value: 0x7f020128
-			public const int notification_template_icon_low_bg = 2130837800;
+			public const int notification_template_icon_bg = 2130837800;
+			
+			// aapt resource value: 0x7f020129
+			public const int notification_template_icon_low_bg = 2130837801;
 			
 			// aapt resource value: 0x7f020123
 			public const int notification_tile_bg = 2130837795;
@@ -2860,6 +2860,9 @@ namespace CasamentoBEC.Droid
 			
 			// aapt resource value: 0x7f020126
 			public const int presente32 = 2130837798;
+			
+			// aapt resource value: 0x7f020127
+			public const int rsvpicon = 2130837799;
 			
 			static Drawable()
 			{
