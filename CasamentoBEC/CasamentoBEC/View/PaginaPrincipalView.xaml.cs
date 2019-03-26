536ed725f7ca0a40f85d90b3b6b78d64d3580f5b
Brian Diego de Souza <Brian-desouza@hotmail.com>
2019-03-25 22:26:31 -0300

    Menu acertado para android, Versao tela pequena, galaxy s8 e simulador.

--- CasamentoBEC/CasamentoBEC/View/PaginaPrincipalView.xaml.cs
+++ CasamentoBEC/CasamentoBEC/View/PaginaPrincipalView.xaml.cs
@@ -14,6 +14,30 @@ namespace CasamentoBEC.View
             InitializeComponent();
             Xamarin.Forms.NavigationPage.SetHasNavigationBar(this, false);//Esconde a navigationbar
             BindingContext = new PaginaPrincipalViewModel();
+            SizeChanged += PaginaPrincipalView_SizeChanged;
+        }
+
+        private void PaginaPrincipalView_SizeChanged(object sender, EventArgs e)
+        {
+            double alturaTela = Height;
+            double larguraTela = Width;
+
+            if(alturaTela <= 546 && larguraTela <= 321 && Device.RuntimePlatform == Device.Android)
+            {
+                var novoTamanhoMenu = new Rectangle(0, -230, 1, 300);
+                AbsoluteLayout.SetLayoutBounds(frameMenu, novoTamanhoMenu);
+                //ResizeMenuLabel();
+            }
+        }
+
+        private void ResizeMenuLabel()
+        {
+            menuContatos.FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label));
+            menuCronograma.FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label));
+            menuFotos.FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label));
+            menuLocal.FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label));
+            menuPresentes.FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label));
+            menuRSVP.FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label));
         }
 
         private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
@@ -39,11 +63,11 @@ namespace CasamentoBEC.View
 
         private void AbrirMenu()
         {
-            frameMenu.TranslateTo(0, 225, 2000, Easing.Linear);
+            frameMenu.TranslateTo(0, 225, 1000, Easing.Linear);
         }
         private void FecharMenu()
         {
-            frameMenu.TranslateTo(0, 0, 2000, Easing.Linear);
+            frameMenu.TranslateTo(0, 0, 1000, Easing.Linear);
         }
 
         
